27c301f3995707da704ad256c6bbef2e87efd8bf
Wojciech Sadowski <w.sadowski@quickersim.com>
2020-02-07 00:27:03 +0100

    mesage stream

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
+#include "msgstream.h"
+
 #include <chrono>
 #include <iostream>
 
-#include "msgstream.h"
+int main( void )
+{
 
-int main(void) {
+    deiimos::verbosity( 1 );
+    deiimos::set_warning( true );
 
-  deiimos::verbosity(1);
-  deiimos::set_warning(false);
+    deiimos::msg( ) << "SILENT" << std::endl;
+    deiimos::msg( 1 ) << "INFO1" << std::endl;
+    deiimos::msg( 2 ) << "INFO2" << std::endl;
+    deiimos::msg( 3 ) << "INFO3" << std::endl;
+    deiimos::err( ) << "Error!" << std::endl;
 
-  deiimos::msg(0) << "SILENT" << std::endl;
-  deiimos::msg(1) << "INFO1" << std::endl;
-  deiimos::msg(2) << "INFO2" << std::endl;
-  deiimos::msg(3) << "INFO3" << std::endl;
-  deiimos::err() << "Error!" << std::endl;
+    deiimos::warn( ) << "Warning" << std::endl;
 
-  deiimos::warn() << "Warning" << std::endl;
-
-  return (0);
+    return ( 0 );
 }
